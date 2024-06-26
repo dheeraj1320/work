416d2269fb10e2b8f7cc70a3ba9e41eea95dfa72
Dheeraj Manwani <dheeraj.manwani@infoorigin.com>
2024-06-26 13:51:22 +0530

    y

---
+++ Untitled-1.js
@@ -0,0 +1,48 @@
+const obj = {
+  "Data Upload Template": [
+    {
+      NAME: "Data Element",
+      IS_LIST: false,
+      IS_MULTIVALUED: false,
+      MASTER_DATA_LABELS: "",
+      MASTER_DATA_VALUES: "",
+    },
+    {
+      NAME: "Is Unique Key",
+      IS_LIST: true,
+      IS_MULTIVALUED: false,
+      MASTER_DATA_LABELS: ["optn1", "optn2","optn3","optn4","optn5","optn6"],
+      MASTER_DATA_VALUES: "",
+    },
+    {
+      NAME: "Is Value Always Required",
+      IS_LIST: true,
+      IS_MULTIVALUED: true,
+      MASTER_DATA_LABELS: ["Input", "Output", "Action"],
+      MASTER_DATA_VALUES: "",
+    },
+  ],
+  "2nd Sheet": [
+    {
+      NAME: "Data Element",
+      IS_LIST: false,
+      IS_MULTIVALUED: false,
+      MASTER_DATA_LABELS: "",
+      MASTER_DATA_VALUES: "",
+    },
+    {
+      NAME: "Is Unique Key",
+      IS_LIST: true,
+      IS_MULTIVALUED: false,
+      MASTER_DATA_LABELS: ["Yes", "No"],
+      MASTER_DATA_VALUES: "",
+    },
+    {
+      NAME: "Is Value Always Required",
+      IS_LIST: true,
+      IS_MULTIVALUED: true,
+      MASTER_DATA_LABELS: ["Yes", "No"],
+      MASTER_DATA_VALUES: "",
+    },
+  ],
+};

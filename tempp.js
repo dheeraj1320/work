d54668472da93440b7df16229dd440b4657debc6
Dheeraj Manwani <dheeraj.manwani@infoorigin.com>
2024-09-25 17:05:45 +0530

    qwer

---
+++ tempp.js
@@ -0,0 +1,30 @@
+if(input[0].Information_label==""){
+  input[0].CHECK=input[0]["PAGE_ACCESS_RELATIVE_URL"];
+}
+
+if (input[0].PAGE_UUID && (input[0].CHECK!=input[0]["PAGE_ACCESS_RELATIVE_URL"])) {
+  input[0].CHECK=input[0]["PAGE_ACCESS_RELATIVE_URL"];
+  
+  if (input[0]["PAGE_ACCESS_RELATIVE_URL"]) {
+    input[0].CHECK=input[0]["PAGE_ACCESS_RELATIVE_URL"];
+    let QuerytoFetchNavigationStep = `select VIEW_NAVIGATION_STEP_UUID from  VIEW_NAVIGATION_STEP where VIEW_NAVIGATION_UUID in (select VIEW_NAVIGATION_UUID from VIEW_NAVIGATION where PAGE_UUID='${input[0]["PAGE_UUID"]}') AND FUNCTIONAL_AREA_UUID = '${input[0]["APP_LOGGED_IN_FUNTIONAL_AREA_ID"]}'`;
+    
+    let QuerytoFetchNavigationStepData = await serviceOrchestrator.selectRecordsUsingQuery(
+      "PRIMARYSPRINGFM",
+      QuerytoFetchNavigationStep,
+      input[0]
+    );
+    
+    let QuerytoFetchNavigationStepRecords = JSON.parse(JSON.stringify(QuerytoFetchNavigationStepData));
+
+    if (QuerytoFetchNavigationStepRecords.length > 0) {
+      input[0].MESSAGE = "As Relative URL is being added to access the page directly, It's Default View Navigation Steps will be deleted.";
+      input[0].Informational_label = "";
+    } else {
+      input[0].Informational_label = "";
+    }
+  }
+  else if(!input[0]["PAGE_ACCESS_RELATIVE_URL"]){
+    input[0].Informational_label = "User can add Navigation Steps to access the page";
+  }
+}

4bfdf494ce78fe44bb7b21c708dc75f9586d1908
Dheeraj Manwani <dheeraj.manwani@infoorigin.com>
2025-02-10 20:09:21 +0530

    a

--- USER_ACTION_OUTCOME.js
+++ USER_ACTION_OUTCOME.js
@@ -1,46 +1,49 @@
 console.log("inside USER_ACTION_OUTCOME composite entity :::::::::::::::: ", input);
 
-const OUTCOME = [];
+const USER_ACTION_OUTCOME = [];
 if(input.isNewOutcomeAdded && input.mode !== 'Edit'){
 
     // Scenario where new outcome is added
-    const OUTCOME_UUID = uuid();
+    const USER_ACTION_OUTCOME_UUID = uuid();
     const outcomeObj = {}
-    outcomeObj.OUTCOME_UUID = OUTCOME_UUID;
+    outcomeObj.USER_ACTION_OUTCOME_UUID = USER_ACTION_OUTCOME_UUID;
     outcomeObj.OUTCOME_NAME = input.title;
+    outcomeObj.PAGE_UUID = input.PAGE_UUID;
+    outcomeObj.USER_ACTION_UUID = input.USER_ACTION_UUID;
+
     outcomeObj.FUNCTIONAL_AREA_UUID = input.APP_LOGGED_IN_FUNTIONAL_AREA_ID;
     outcomeObj.AE_INSERT_ID = input.APP_LOGGED_IN_USER_ID;
     
-    OUTCOME.push(outcomeObj);
-    input['OUTCOME_UUID'] = OUTCOME_UUID;
+    USER_ACTION_OUTCOME.push(outcomeObj);
+    input['USER_ACTION_OUTCOME_UUID'] = USER_ACTION_OUTCOME_UUID;
 } else if (input.mode === 'Edit') {
     // Scenario where outcome name has changed
-    const outcomeQuery = `SELECT * FROM USER_ACTION_OUTCOME WHERE USER_ACTION_OUTCOME_UUID = '${input.USER_ACTION_OUTCOME_UUID}' AND FUNCTIONAL_AREA_UUID = '${input.APP_LOGGED_IN_FUNTIONAL_AREA_ID}'`;
+    const outcomeQuery = `SELECT * FROM PROCESS_OUTCOME WHERE PROCESS_OUTCOME_UUID = '${input.PROCESS_OUTCOME_UUID}' AND FUNCTIONAL_AREA_UUID = '${input.APP_LOGGED_IN_FUNTIONAL_AREA_ID}'`;
     const outcomeQueryData = await serviceOrchestrator.selectRecordsUsingQuery('PRIMARYSPRINGFM', outcomeQuery, input);
 
     if(outcomeQueryData && outcomeQueryData[0]){
-        input['OUTCOME_UUID'] = outcomeQueryData[0].OUTCOME_UUID;
+        input['USER_ACTION_OUTCOME_UUID'] = outcomeQueryData[0].USER_ACTION_OUTCOME_UUID;
         
         const outcomeObj = {}
-        outcomeObj.OUTCOME_UUID = outcomeQueryData[0].OUTCOME_UUID;
+        outcomeObj.USER_ACTION_OUTCOME_UUID = outcomeQueryData[0].USER_ACTION_OUTCOME_UUID;
         outcomeObj.OUTCOME_NAME = input.title;
 
-        OUTCOME.push(outcomeObj);
+        USER_ACTION_OUTCOME.push(outcomeObj);
     }
 } else if(input.actionName === 'Delete'){
     // Scenario where an outcome is deleted
-    const outcomeQuery = `SELECT * FROM USER_ACTION_OUTCOME WHERE USER_ACTION_OUTCOME_UUID = '${input.USER_ACTION_OUTCOME_UUID}' AND FUNCTIONAL_AREA_UUID = '${input.APP_LOGGED_IN_FUNTIONAL_AREA_ID}'`;
+    const outcomeQuery = `SELECT * FROM PROCESS_OUTCOME WHERE PROCESS_OUTCOME_UUID = '${input.PROCESS_OUTCOME_UUID}' AND FUNCTIONAL_AREA_UUID = '${input.APP_LOGGED_IN_FUNTIONAL_AREA_ID}'`;
     const outcomeQueryData = await serviceOrchestrator.selectRecordsUsingQuery('PRIMARYSPRINGFM', outcomeQuery, input);
 
-    const allOutcomesQuery = `SELECT * FROM USER_ACTION_OUTCOME WHERE OUTCOME_UUID = '${outcomeQueryData[0].OUTCOME_UUID}' AND FUNCTIONAL_AREA_UUID = '${input.APP_LOGGED_IN_FUNTIONAL_AREA_ID}'`;
+    const allOutcomesQuery = `SELECT * FROM PROCESS_OUTCOME WHERE USER_ACTION_OUTCOME_UUID = '${outcomeQueryData[0].USER_ACTION_OUTCOME_UUID}' AND FUNCTIONAL_AREA_UUID = '${input.APP_LOGGED_IN_FUNTIONAL_AREA_ID}'`;
     const allOutcomesQueryData = await serviceOrchestrator.selectRecordsUsingQuery('PRIMARYSPRINGFM', allOutcomesQuery, input);
 
     if(allOutcomesQueryData.length === 1){
-        OUTCOME.push({
-            OUTCOME_UUID: outcomeQueryData[0].OUTCOME_UUID,
+        USER_ACTION_OUTCOME.push({
+            USER_ACTION_OUTCOME_UUID: outcomeQueryData[0].USER_ACTION_OUTCOME_UUID,
             compositeEntityAction: "Delete"
         });
     }
 }
 
-input['AppEngChildEntity:OUTCOME'] = OUTCOME;
+input['AppEngChildEntity:USER_ACTION_OUTCOME'] = USER_ACTION_OUTCOME;

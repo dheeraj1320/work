ab3d3a9c648b2c59f172b4cdb31fad327347f3b8
Dheeraj Manwani <dheeraj.manwani@infoorigin.com>
2025-02-12 11:19:22 +0530

    qwErtty

--- Create UI Element.js
+++ Create UI Element.js
@@ -7,29 +7,24 @@ try {
     if (input[0]['DATA_SET_UUID']) {
       let keyForName = '';
       input[0]['INFO_MESSAGE'] = '';
-      let QuerytoFetch = `select DATA_ELEMENT_UUID from DATA_ELEMENT where DATA_SET_UUID=:DATA_SET_UUID and FUNCTIONAL_AREA_UUID = :APP_LOGGED_IN_FUNTIONAL_AREA_ID`;
-      let QuerytoFetchData = await serviceOrchestrator.selectRecordsUsingQuery(
-        'PRIMARYSPRINGFM',
-        QuerytoFetch,
-        input[0]
-      );
-      let QuerytoFetchRecords = JSON.parse(JSON.stringify(QuerytoFetchData));
       let Query = `select * from DATA_ELEMENT where DATA_SET_UUID=:DATA_SET_UUID and FUNCTIONAL_AREA_UUID = :APP_LOGGED_IN_FUNTIONAL_AREA_ID order by DATA_ELEMENT_ID asc`;
       let Query1 = await serviceOrchestrator.selectRecordsUsingQuery('PRIMARYSPRINGFM', Query, input[0]);
       let Query2 = JSON.parse(JSON.stringify(Query1));
       input[0].child['073cd693-8b19-415f-ad44-808cba68a641'] = [];
-      if (QuerytoFetchRecords.length > 0) {
+      if (Query2.length > 0) {
         input[0]['INFO_MESSAGE'] = '';
         i = 0;
-        for (let DATA_ELEMENT_UUID of QuerytoFetchRecords) {
-          input[0].child['073cd693-8b19-415f-ad44-808cba68a641'][i] = DATA_ELEMENT_UUID;
-          input[0].child['073cd693-8b19-415f-ad44-808cba68a641'][i]['UI_ELEMENT_NAME'] = Query2[i].DATA_ELEMENT_NAME;
+        for (let obj of Query2) {
+          input[0].child['073cd693-8b19-415f-ad44-808cba68a641'][i] = {};
+          input[0].child['073cd693-8b19-415f-ad44-808cba68a641'][i]['DATA_ELEMENT_UUID'] = obj.DATA_ELEMENT_UUID;
+          input[0].child['073cd693-8b19-415f-ad44-808cba68a641'][i]['UI_ELEMENT_NAME'] = obj.DATA_ELEMENT_NAME;
           input[0].child['073cd693-8b19-415f-ad44-808cba68a641'][i]['LOCATOR_TYPE'] = 'Selector';
           input[0].child['073cd693-8b19-415f-ad44-808cba68a641'][i]['PAGE_NEW_UUID'] = input[0]['PAGE_NEW_UUID'];
           input[0].child['073cd693-8b19-415f-ad44-808cba68a641'][i]['isNew'] = true;
+          console.log('Data after setting === >>> ', input[0].child['073cd693-8b19-415f-ad44-808cba68a641'][i]);
           i++;
         }
-        for (let k = 0; k < QuerytoFetchRecords.length; k++) {
+        for (let k = 0; k < Query2.length; k++) {
           keyForName =
             keyForName +
             `583bf4ff-1db5-4422-a1cd-b32bf1a2d1cc=>0=>9b9cff6f-f5a6-480e-b2be-a12c07f9446d=>${k}=>UI_ELEMENT_NAME` +
---
+++ Data Set Warning.js
@@ -0,0 +1,55 @@
+try {
+  console.log('Data set Action Flow');
+  let input = msg.payload.apiRequestBody.baseEntity.records[0];
+  console.log('inputtttttt', input);
+  let AppengProcessConfig = global.get('AppengProcessConfig');
+  const serviceOrchestrator = AppengProcessConfig.serviceOrchestrator;
+  console.log('value in payload == == == ', msg.payload);
+  let queryToFetchCount = `select COUNT(*) AS COUNT from UI_ELEMENT where DATA_SET_UUID = '${input.DATA_SET_UUID}' AND FUNCTIONAL_AREA_UUID = '${input.APP_LOGGED_IN_FUNTIONAL_AREA_ID}'`;
+  let queryToFetchCountData = await serviceOrchestrator.selectRecordsUsingQuery(
+    'PRIMARYSPRINGFM',
+    queryToFetchCount,
+    input
+  );
+  console.log('query result === ', queryToFetchCountData);
+  if (queryToFetchCountData[0].COUNT > 0) {
+    console.log('SETTED THE OPEN MODAL TO TRUE');
+    msg.payload['isOpenModal'] = true;
+  }
+  console.log('msg:::::::::::::::::::', msg.payload['isOpenModal']);
+  node.send(msg);
+} catch (error) {
+  console.log('Errorr Occured ', error.message);
+}
+return;
+
+// ----------------------------------------------
+
+let atr = msg.payload.apiRequestBody;
+let routeStateParams = {};
+let input = msg.payload.apiRequestBody.baseEntity.records;
+const updateWarningPortal = '0_1cd1075c-c650-4f8c-bea0-3aa8adde9db2';
+const deleteWarningPortal = '0_c857d993-3106-4941-8e9b-b55578a35700';
+let portalToOpen = msg.payload.apiRequestBody.action === 'Update' ? updateWarningPortal : deleteWarningPortal;
+routeStateParams.portalId = portalToOpen;
+atr.cleanData = portalToOpen;
+atr.routeStateParams = routeStateParams;
+msg.payload.result = {
+  mode: 'Enable Message',
+  message: 'Please Wait..',
+  attributes: atr,
+  navigation: { operationType: 'OpenModal', portalId: portalToOpen },
+};
+msg.payload.result.modifyOtherCard = {};
+msg.payload.result.modifyOtherCard[portalToOpen] = [
+  {
+    parameter: 'referenceData',
+    parentId: portalToOpen,
+    portalId: portalToOpen,
+    parameterKey: 'data',
+    type: 'SubPortal',
+    eventType: 'uploadGridData',
+    data: input[0],
+  },
+];
+return msg;

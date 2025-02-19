d4e1f7b2f3c8856fe16f0fb67ee43c29b38c261e
Dheeraj Manwani <dheeraj.manwani@infoorigin.com>
2025-02-19 12:38:02 +0530

    pp

---
+++ Delete message for data set and data element.js
@@ -0,0 +1,11 @@
+try {
+    let input = msg.payload.apiRequestBody.baseEntity.records;
+    msg.payload.result['mode'] = 'Insert';
+    msg.payload.result['pk'] = '';
+    msg.payload.result['message'] = 'Deleted Successfully';
+    node.send(msg);
+  } catch (error) {
+    console.log('Errorr Occured in Delete message for impacted process', error.message);
+  }
+  return;
+  

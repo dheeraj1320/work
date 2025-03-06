4b3b0924e33284dbe020a9e1cb32d1078a3fdffc
Dheeraj-at-381491913668 <dheeraj.manwani@infoorigin.com>
2025-03-06 14:10:42 +0530

    qq

--- View_UI_Element.js
+++ View_UI_Element.js
@@ -1,42 +1,104 @@
-const VIEW_UI_ELEMENT = []
+let viewUIElementList = [];
+let viewUIElementApplicableStep = [];
 
-
-function deleteRecord(primarykey, primarykeyvalue, tablename, functionalareauuid) {
-  
+function deleteRecord(primarykey, primarykeyvalue, tablename) {
     let deleteTableData = {};
-
     deleteTableData[primarykey] = primarykeyvalue;
     deleteTableData["compositeEntityAction"] = "Delete";
-    deleteTableData["FUNCTIONAL_AREA_UUID"] = functionalareauuid;
-    
+    deleteTableData["FUNCTIONAL_AREA_UUID"] = input.APP_LOGGED_IN_FUNTIONAL_AREA_ID;
     if (tablename == 'VIEW_UI_ELEMENT') {
-        VIEW_UI_ELEMENT.push(deleteTableData);
+        viewUIElementList.push(deleteTableData);
+    } else if (tablename == 'VIEW_UI_ELEMENT_APPLICABLE_STEP') {
+        viewUIElementApplicableStep.push(deleteTableData);
     }
 }
 
+function isValidUUID(uuid) {
+    let uuidRegex = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
+    return uuidRegex.test(uuid);
+}
+
+function appendCommas(list) {
+    if (list && list.length) {
+        return [...new Set(list.map(item => item).filter(isValidUUID))].map(uuid => `'${uuid}'`).join(', ');
+    } else {
+        return '';
+    }
+}
+
+function getViewUIElementIds(list, uiElementIds) {
+    let result = list.filter((item) => item['UI_ELEMENT_UUID'] == uiElementIds);
+    if (result && result.length) {
+        return result[0]['VIEW_UI_ELEMENT_UUID'];
+    } else {
+        return '';
+    }
+}
 
 if (input.compositeEntityAction == 'Save' && input.VIEW_UI_ELEMENT_NAME) {
+    let uiElementUuids = input.VIEW_UI_ELEMENT_NAME.split(',');
+    for (let id of uiElementUuids) {
+        let viewUIElementObject = {};
+        let viewUIElementUUID = uuid();
+        viewUIElementObject['VIEW_UUID'] = input.VIEW_UUID;
+        viewUIElementObject['PAGE_UUID'] = input.PAGE_UUID;
+        viewUIElementObject['UI_ELEMENT_UUID'] = id;
+        viewUIElementObject['VIEW_UI_ELEMENT_UUID'] = viewUIElementUUID;
+        viewUIElementList.push(viewUIElementObject);
+    }
 
-    const uiElementUuids = input.VIEW_UI_ELEMENT_NAME.split(',');
+    let stepDefinitionBasedOnMode = [{
+        primaryMode: 'Input',
+        secondaryMode: 'User Action',
+        isLike: false,
+        stepFilter: 'Input Step'
+    }, {
+        primaryMode: 'User Action',
+        secondaryMode: 'Input',
+        isLike: true,
+        stepFilter: 'Input and Action Step'
+    }, {
+        primaryMode: 'User Action',
+        secondaryMode: 'Input',
+        isLike: false,
+        stepFilter: 'Action Step'
+    }, {
+        primaryMode: 'Output',
+        secondaryMode: 'Output',
+        isLike: true,
+        stepFilter: 'Expected Result Step'
+    }];
 
-    for(id of uiElementUuids){
-        const viewUiEl = {};
 
-        viewUiEl['VIEW_UI_ELEMENT_UUID'] = uuid();
-        viewUiEl['VIEW_UUID'] = input.VIEW_UUID;
-        viewUiEl['PAGE_UUID'] = input.PAGE_UUID;
-        viewUiEl['UI_ELEMENT_UUID'] = id;
-        
-        VIEW_UI_ELEMENT.push(viewUiEl);
+    let uiElementIds = appendCommas(input['VIEW_UI_ELEMENT_NAME'] ? input['VIEW_UI_ELEMENT_NAME'].split(',') : []);
+
+    for (let data of stepDefinitionBasedOnMode) {
+        let uiElementQuery = `SELECT ue.UI_ELEMENT_UUID, UI_ELEMENT_ID, UI_ELEMENT_NAME, UI_ELEMENT_TYPE_NAME, UI_ELEMENT_TYPE_ID, UI_ELEMENT_TYPE, LOCATOR_TYPE, LOCATOR_VALUE, IS_PAGE_IDENTIFIER, EVENT_NAME, PAGE_NEW_UUID, ue.FUNCTIONAL_AREA_UUID, UI_ELEMENT_MODE FROM UI_ELEMENT ue, UI_ELEMENT_TYPE_MASTER uetm WHERE ue.UI_ELEMENT_TYPE = uetm.UI_ELEMENT_TYPE_UUID AND ue.UI_ELEMENT_UUID in (${uiElementIds ? uiElementIds : `' '`}) AND ue.FUNCTIONAL_AREA_UUID=:APP_LOGGED_IN_FUNTIONAL_AREA_ID AND CONCAT(',', UI_ELEMENT_MODE, ',') LIKE CONCAT( '%,', ${`'` + data['primaryMode'] + `'`}, ',%' ) AND CONCAT(',', UI_ELEMENT_MODE, ',') ${data['isLike'] ? ' LIKE ' : ' NOT LIKE '} CONCAT( '%,', ${`'` + data['secondaryMode'] + `'`}, ',%' ) order by UI_ELEMENT_ID asc`;
+        let stepDefinitionFilter = `'` + data['stepFilter'] + `'`;
+        let uiElementQueryData = await serviceOrchestrator.selectRecordsUsingQuery('PRIMARYSPRINGFM', uiElementQuery, input);
+        for (let uiElement of uiElementQueryData) {
+            let uIElementType = `'` + uiElement['UI_ELEMENT_TYPE_NAME'] + `'`;
+            let stepDefTemplateVerbaleQuery = ['Input', 'User Action'].includes(data['primaryMode']) && ['Input Step', 'Input and Action Step'].includes(data['stepFilter']) ? `SELECT * FROM STEP_DEFINITION_TEMPLATE_VERBIAGE WHERE STEP_FILTER in (${stepDefinitionFilter}) AND IS_ACTIVE_STEP_DEFINITION_TEMPLATE_VERBIAGE = 'Yes' and APPLICABLE_UI_ELEMENT_TYPE in (${uIElementType})` : `SELECT * FROM STEP_DEFINITION_TEMPLATE_VERBIAGE WHERE STEP_FILTER in(${stepDefinitionFilter}) AND IS_ACTIVE_STEP_DEFINITION_TEMPLATE_VERBIAGE = 'Yes'`;
+            let stepDefinationData = await serviceOrchestrator.selectRecordsUsingQuery('PRIMARYSPRINGFM', stepDefTemplateVerbaleQuery, input);
+            for (let stepDefData of stepDefinationData) {
+                let viewUIElementApplicableStepObject = {};
+                viewUIElementApplicableStepObject['VIEW_UI_ELEMENT_UUID'] = getViewUIElementIds(viewUIElementList, uiElement['UI_ELEMENT_UUID']);
+                viewUIElementApplicableStepObject['STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID'] = stepDefData['STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID'];
+                viewUIElementApplicableStep.push(viewUIElementApplicableStepObject);
+            }
+        }
     }
 
 }
 
-
 if (input.compositeEntityAction == 'Delete') {
-    deleteRecord("VIEW_UI_ELEMENT_UUID", input.VIEW_UI_ELEMENT_UUID, "VIEW_UI_ELEMENT", input.APP_LOGGED_IN_FUNTIONAL_AREA_ID)
+    deleteRecord("VIEW_UI_ELEMENT_UUID", input.VIEW_UI_ELEMENT_UUID, "VIEW_UI_ELEMENT");
+    let applicableStepViewUIElementQuery = `SELECT * FROM VIEW_UI_ELEMENT_APPLICABLE_STEP WHERE VIEW_UI_ELEMENT_UUID=:VIEW_UI_ELEMENT_UUID AND FUNCTIONAL_AREA_UUID=:APP_LOGGED_IN_FUNTIONAL_AREA_ID`;
+    let applicableStepViewUIElementQueryData = await serviceOrchestrator.selectRecordsUsingQuery('PRIMARYSPRINGFM', applicableStepViewUIElementQuery, input);
+    for (let data of applicableStepViewUIElementQueryData) {
+        deleteRecord("VIEW_UI_ELEMENT_APPLICABLE_STEP_UUID", data['VIEW_UI_ELEMENT_APPLICABLE_STEP_UUID'], "VIEW_UI_ELEMENT_APPLICABLE_STEP");
+    }
 }
 
-console.log("Data to push ====================== ", VIEW_UI_ELEMENT);
-
-input["AppEngChildEntity:VIEW_UI_ELEMENT_CHILD_OF_VIEW_UI_ELEMENT"] = VIEW_UI_ELEMENT;
+input["AppEngChildEntity:VIEW_UI_ELEMENT_CHILD_OF_VIEW_UI_ELEMENT"] = viewUIElementList;
+input["AppEngChildEntity:VIEW_UI_ELEMENT_APPLICABLE_STEP"] = viewUIElementApplicableStep;

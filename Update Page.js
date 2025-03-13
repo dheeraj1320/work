81a246f82ce706e6a604a0ca1c74ebe92e8e4cf6
Dheeraj-at-381491913668 <dheeraj.manwani@infoorigin.com>
2025-03-13 16:34:07 +0530

    qpdfo

--- Update Page.js
+++ Update Page.js
@@ -1,151 +1,226 @@
-console.log("Page node business rule ========================== >>>>>>>>>>>> ", input)
-
-let UI_ELEMENT = [];
-let VIEW_UI_ELEMENT = [];
-let VIEW_NAVIGATION_STEP = [];
-let VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE = [];
-let pageViewList = [];
-let testSetlist = [];
-
-function deleteRecord(primarykey, primarykeyvalue, tablename, functionalareauuid) {
-
-  let deleteTableData = {};
-
-  deleteTableData[primarykey] = primarykeyvalue;
-  deleteTableData["compositeEntityAction"] = "Delete";
-  deleteTableData["FUNCTIONAL_AREA_UUID"] = functionalareauuid;
-
-  if (tablename == 'UI_ELEMENT') {
-    UI_ELEMENT.push(deleteTableData);
-  } else if (tablename == 'VIEW_UI_ELEMENT') {
-    VIEW_UI_ELEMENT.push(deleteTableData);
-  } else if (tablename == 'VIEW_NAVIGATION_STEP') {
-    VIEW_NAVIGATION_STEP.push(deleteTableData);
-  } else if (tablename == 'VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE') {
-    VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE.push(deleteTableData);
-  } else if (tablename == 'PAGE_VIEW') {
-    pageViewList.push(deleteTableData);
-  }
-}
-
 if (input.compositeEntityAction == "Update") {
+  
+  console.log('NodeBusinessRule Started');
+  let VIEW_NAVIGATION_STEP = [];
+  let VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE = [];
+
+  function deleteRecord(
+      primarykey,
+      primarykeyvalue,
+      tablename,
+      functionalareauuid
+  ) {
+      let deleteTableData = {};
+      deleteTableData[primarykey] = primarykeyvalue;
+      deleteTableData["compositeEntityAction"] = "Delete";
+      deleteTableData["FUNCTIONAL_AREA_UUID"] = functionalareauuid;
+
+      if (tablename == "VIEW_NAVIGATION_STEP") {
+          VIEW_NAVIGATION_STEP.push(deleteTableData);
+      } else if (tablename == "VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE") {
+          VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE.push(deleteTableData);
+      }
+  }
+
+  // Delete Records
   input["Informational_label"] = "";
-  let testCaseStepList = [];
-  let testCaseFunctionStepList = [];
-  let functionStepList = [];
-  let pageQuery = `select * FROM PAGE WHERE PAGE_UUID=:PAGE_UUID AND FUNCTIONAL_AREA_UUID=:APP_LOGGED_IN_FUNTIONAL_AREA_ID`;
-  let pageQueryData = await serviceOrchestrator.selectSingleRecordUsingQuery("PRIMARYSPRINGFM", pageQuery, input);
+  if (input["PAGE_ACCESS_RELATIVE_URL"]) {
+      //for VIEW_NAVIGATION_STEP
+      let QuerytoFetchNavigationStep = `select VIEW_NAVIGATION_STEP_UUID from VIEW_NAVIGATION_STEP where VIEW_UUID in (select VIEW_UUID from PAGE_VIEW where PAGE_UUID=:PAGE_UUID AND IS_DEFAULT_VIEW = 'Yes') AND FUNCTIONAL_AREA_UUID = :APP_LOGGED_IN_FUNTIONAL_AREA_ID`;
+      let QuerytoFetchNavigationStepData =
+          await serviceOrchestrator.selectRecordsUsingQuery(
+              "PRIMARYSPRINGFM",
+              QuerytoFetchNavigationStep,
+              input
+          );
+      let QuerytoFetchNavigationStepRecords = JSON.parse(
+          JSON.stringify(QuerytoFetchNavigationStepData),
+      );
 
-  async function updateIsPureNavigationStep(isPureNavigationStep) {
-    let testCaseStepQuery = `select tcs.TEST_CASE_STEP_UUID from STEP_DEFINITION_TEMPLATE_VERBIAGE sdtv join TEST_CASE_STEP tcs on sdtv.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID = tcs.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID where tcs.CURRENT_PAGE_CONTEXT=:PAGE_UUID and sdtv.IS_PURE_NAVIGATION_STEP='Yes'`;
+      //for VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE table
+      let QuerytoFetchNavigationStepAttribute = `select VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE_UUID from  VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE where VIEW_UUID in (select VIEW_UUID from PAGE_VIEW where PAGE_UUID=:PAGE_UUID AND IS_DEFAULT_VIEW = 'Yes') AND FUNCTIONAL_AREA_UUID = :APP_LOGGED_IN_FUNTIONAL_AREA_ID`;
+      let QuerytoFetchNavigationStepAttributeData =
+          await serviceOrchestrator.selectRecordsUsingQuery(
+              "PRIMARYSPRINGFM",
+              QuerytoFetchNavigationStepAttribute,
+              input
+          );
+      let QuerytoFetchNavigationStepAttributeDataRecords = JSON.parse(JSON.stringify(QuerytoFetchNavigationStepAttributeData));
 
-    let testCaseStepQueryData = await serviceOrchestrator.selectRecordsUsingQuery("PRIMARYSPRINGFM", testCaseStepQuery, input);
+      if (QuerytoFetchNavigationStepRecords.length > 0) {
+          for (let key in QuerytoFetchNavigationStepRecords) {
+              deleteRecord("VIEW_NAVIGATION_STEP_UUID",QuerytoFetchNavigationStepRecords[key].VIEW_NAVIGATION_STEP_UUID,"VIEW_NAVIGATION_STEP",input["APP_LOGGED_IN_FUNTIONAL_AREA_ID"]);
+          }
+      }
 
-    for (let testCaseStep of testCaseStepQueryData) {
-      let data = {};
-      data["TEST_CASE_STEP_UUID"] = testCaseStep['TEST_CASE_STEP_UUID'];
-      data["IS_PURE_NAVIGATION_STEP"] = isPureNavigationStep;
-      testCaseStepList.push(data);
-    }
+      if (QuerytoFetchNavigationStepAttributeDataRecords.length > 0) {
+          for (let key in QuerytoFetchNavigationStepAttributeDataRecords) {
+              deleteRecord(
+                  "VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE_UUID",
+                  QuerytoFetchNavigationStepAttributeDataRecords[key].VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE_UUID,
+                  "VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE",
+                  input["APP_LOGGED_IN_FUNTIONAL_AREA_ID"]
+              );
+          }
+      }
 
-    let testCaseFunctionStepQuery = `select tcfs.TEST_CASE_FUNCTION_STEP_UUID from STEP_DEFINITION_TEMPLATE_VERBIAGE sdtv join TEST_CASE_FUNCTION_STEP tcfs on sdtv.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID = tcfs.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID where tcfs.CURRENT_PAGE_CONTEXT=:PAGE_UUID and sdtv.IS_PURE_NAVIGATION_STEP='Yes'`;
+      // Update IS_PURE_NAVIGATION_STEP for TEST_CASE_STEP
+      let QueryToFetchTestCaseStep = `select tcs.* from STEP_DEFINITION_TEMPLATE_VERBIAGE sdtv join TEST_CASE_STEP tcs on sdtv.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID = tcs.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID where tcs.CURRENT_PAGE_CONTEXT=:PAGE_UUID and sdtv.IS_PURE_NAVIGATION_STEP='Yes' `;
 
-    let testCaseFunctionStepQueryData = await serviceOrchestrator.selectRecordsUsingQuery("PRIMARYSPRINGFM", testCaseFunctionStepQuery, input);
+      let QueryToFetchTestCaseStepData =
+          await serviceOrchestrator.selectRecordsUsingQuery(
+              "PRIMARYSPRINGFM",
+              QueryToFetchTestCaseStep,
+              input
+          );
+      let QuerytoFetchTestCaseStepRecords = JSON.parse(JSON.stringify(QueryToFetchTestCaseStepData));
 
-    for (let testCaseFunctionStep of testCaseFunctionStepQueryData) {
-      let data = {};
-      data["TEST_CASE_FUNCTION_STEP_UUID"] = testCaseFunctionStep['TEST_CASE_FUNCTION_STEP_UUID'];
-      data["IS_PURE_NAVIGATION_STEP"] = isPureNavigationStep;
-      testCaseFunctionStepList.push(data);
-    }
+      let TestCaseStep = [];
+      for (let key in QuerytoFetchTestCaseStepRecords) {
+          let data = {};
 
-    let functionStepQuery = `select fs.FUNCTION_STEP_UUID from STEP_DEFINITION_TEMPLATE_VERBIAGE sdtv 
-        join FUNCTION_STEP fs on sdtv.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID = fs.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID where fs.CURRENT_PAGE_CONTEXT=:PAGE_UUID and sdtv.IS_PURE_NAVIGATION_STEP='Yes'`;
+          data["TEST_CASE_STEP_UUID"] =
+              QuerytoFetchTestCaseStepRecords[key].TEST_CASE_STEP_UUID;
+          data["IS_PURE_NAVIGATION_STEP"] = "No";
 
-    let functionStepQueryData = await serviceOrchestrator.selectRecordsUsingQuery("PRIMARYSPRINGFM", functionStepQuery, input);
+          TestCaseStep.push(data);
+      }
 
-    for (let functionStep of functionStepQueryData) {
-      let data = {};
-      data["FUNCTION_STEP_UUID"] = functionStep['FUNCTION_STEP_UUID'];
-      data["IS_PURE_NAVIGATION_STEP"] = isPureNavigationStep;
-      functionStepList.push(data);
-    }
+      // Update IS_PURE_NAVIGATION_STEP for TEST_CASE_FUNCTION_STEP
+      let QueryToFetchTestCaseFunctionStep = `
+          select tcfs.TEST_CASE_FUNCTION_STEP_UUID from STEP_DEFINITION_TEMPLATE_VERBIAGE sdtv join TEST_CASE_FUNCTION_STEP tcfs on sdtv.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID = tcfs.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID where tcfs.CURRENT_PAGE_CONTEXT=:PAGE_UUID and sdtv.IS_PURE_NAVIGATION_STEP='Yes' `;
+
+      let QueryToFetchTestCaseFunctionStepData =
+          await serviceOrchestrator.selectRecordsUsingQuery(
+              "PRIMARYSPRINGFM",
+              QueryToFetchTestCaseFunctionStep,
+              input
+          );
+      let QuerytoFetchTestCaseFunctionStepRecords = JSON.parse(JSON.stringify(QueryToFetchTestCaseFunctionStepData));
+
+      let TestCaseFunctionStep = [];
+
+      for (let key in QuerytoFetchTestCaseFunctionStepRecords) {
+          let data = {};
+          data["TEST_CASE_FUNCTION_STEP_UUID"] =
+              QuerytoFetchTestCaseFunctionStepRecords[
+                  key
+              ].TEST_CASE_FUNCTION_STEP_UUID;
+          data["IS_PURE_NAVIGATION_STEP"] = "No";
+
+          TestCaseFunctionStep.push(data);
+      }
+
+      // Update IS_PURE_NAVIGATION_STEP for FUNCTION_STEP
+      let QueryToFetchFunctionStep = `
+      select fs.FUNCTION_STEP_UUID from STEP_DEFINITION_TEMPLATE_VERBIAGE sdtv 
+      join FUNCTION_STEP fs on sdtv.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID = fs.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID where fs.CURRENT_PAGE_CONTEXT=:PAGE_UUID and sdtv.IS_PURE_NAVIGATION_STEP='Yes' `;
+
+      let QueryToFetchFunctionStepData =
+          await serviceOrchestrator.selectRecordsUsingQuery(
+              "PRIMARYSPRINGFM",
+              QueryToFetchFunctionStep,
+              input
+          );
+      let QuerytoFetchFunctionStepRecords = JSON.parse(JSON.stringify(QueryToFetchFunctionStepData));
+
+      let FunctionStep = [];
+
+      for (let key in QuerytoFetchFunctionStepRecords) {
+          let data = {};
+          data["FUNCTION_STEP_UUID"] =
+              QuerytoFetchFunctionStepRecords[key].FUNCTION_STEP_UUID;
+          data["IS_PURE_NAVIGATION_STEP"] = "No";
+
+          FunctionStep.push(data);
+      }
+
+      input["AppEngChildEntity:TEST_CASE_STEP_NEW"] = TestCaseStep;
+      input["AppEngChildEntity:TEST_CASE_FUNCTION_STEP"] = TestCaseFunctionStep;
+      input["AppEngChildEntity:FUNCTION_STEP"] = FunctionStep;
+  } else {
+      // Update IS_PURE_NAVIGATION_STEP for TEST_CASE_STEP
+      let QueryToFetchTestCaseStep = `select tcs.TEST_CASE_STEP_UUID from STEP_DEFINITION_TEMPLATE_VERBIAGE sdtv join TEST_CASE_STEP tcs on sdtv.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID = tcs.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID where tcs.CURRENT_PAGE_CONTEXT=:PAGE_UUID and sdtv.IS_PURE_NAVIGATION_STEP='Yes' `;
+
+      let QueryToFetchTestCaseStepData =
+          await serviceOrchestrator.selectRecordsUsingQuery(
+              "PRIMARYSPRINGFM",
+              QueryToFetchTestCaseStep,
+              input
+          );
+      let QuerytoFetchTestCaseStepRecords = JSON.parse(
+          JSON.stringify(QueryToFetchTestCaseStepData),
+      );
+
+      let TestCaseStep = [];
+
+      for (let key in QuerytoFetchTestCaseStepRecords) {
+          let data = {};
+          data["TEST_CASE_STEP_UUID"] =
+              QuerytoFetchTestCaseStepRecords[key].TEST_CASE_STEP_UUID;
+          data["IS_PURE_NAVIGATION_STEP"] = "Yes";
+
+          TestCaseStep.push(data);
+      }
+
+      // Update IS_PURE_NAVIGATION_STEP for TEST_CASE_FUNCTION_STEP
+      let QueryToFetchTestCaseFunctionStep = `
+          select tcfs.TEST_CASE_FUNCTION_STEP_UUID from STEP_DEFINITION_TEMPLATE_VERBIAGE sdtv join TEST_CASE_FUNCTION_STEP tcfs on sdtv.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID = tcfs.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID where tcfs.CURRENT_PAGE_CONTEXT=:PAGE_UUID and sdtv.IS_PURE_NAVIGATION_STEP='Yes' `;
+
+      let QueryToFetchTestCaseFunctionStepData =
+          await serviceOrchestrator.selectRecordsUsingQuery(
+              "PRIMARYSPRINGFM",
+              QueryToFetchTestCaseFunctionStep,
+              input
+          );
+      let QuerytoFetchTestCaseFunctionStepRecords = JSON.parse(
+          JSON.stringify(QueryToFetchTestCaseFunctionStepData),
+      );
+
+      let TestCaseFunctionStep = [];
+
+      for (let key in QuerytoFetchTestCaseFunctionStepRecords) {
+          let data = {};
+          data["TEST_CASE_FUNCTION_STEP_UUID"] =
+              QuerytoFetchTestCaseFunctionStepRecords[
+                  key
+              ].TEST_CASE_FUNCTION_STEP_UUID;
+          data["IS_PURE_NAVIGATION_STEP"] = "Yes";
+
+          TestCaseFunctionStep.push(data);
+      }
+
+      // Update IS_PURE_NAVIGATION_STEP for FUNCTION_STEP
+      let QueryToFetchFunctionStep = `
+      select fs.FUNCTION_STEP_UUID from STEP_DEFINITION_TEMPLATE_VERBIAGE sdtv 
+      join FUNCTION_STEP fs on sdtv.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID = fs.STEP_DEFINITION_TEMPLATE_VERBIAGE_UUID where fs.CURRENT_PAGE_CONTEXT=:PAGE_UUID and sdtv.IS_PURE_NAVIGATION_STEP='Yes' `;
+
+      let QueryToFetchFunctionStepData =
+          await serviceOrchestrator.selectRecordsUsingQuery(
+              "PRIMARYSPRINGFM",
+              QueryToFetchFunctionStep,
+              input
+          );
+      let QuerytoFetchFunctionStepRecords = JSON.parse(
+          JSON.stringify(QueryToFetchFunctionStepData),
+      );
+
+      let FunctionStep = [];
+
+      for (let key in QuerytoFetchFunctionStepRecords) {
+          let data = {};
+          data["FUNCTION_STEP_UUID"] =
+              QuerytoFetchFunctionStepRecords[key].FUNCTION_STEP_UUID;
+          data["IS_PURE_NAVIGATION_STEP"] = "Yes";
+
+          FunctionStep.push(data);
+      }
+
+      input["AppEngChildEntity:TEST_CASE_STEP_NEW"] = TestCaseStep;
+      input["AppEngChildEntity:TEST_CASE_FUNCTION_STEP"] = TestCaseFunctionStep;
+      input["AppEngChildEntity:FUNCTION_STEP"] = FunctionStep;
   }
 
-  if (pageQueryData['PAGE_ACCESS_RELATIVE_URL'] == input["PAGE_ACCESS_RELATIVE_URL"] || (pageQueryData['PAGE_ACCESS_RELATIVE_URL'] && input["PAGE_ACCESS_RELATIVE_URL"]) || (!pageQueryData['PAGE_ACCESS_RELATIVE_URL'] && !input["PAGE_ACCESS_RELATIVE_URL"])) {
-    console.log('No data need to modify.');
-  } else if (pageQueryData['PAGE_ACCESS_RELATIVE_URL'] != input["PAGE_ACCESS_RELATIVE_URL"] && input["PAGE_ACCESS_RELATIVE_URL"].length > 0) {
-    await updateIsPureNavigationStep('No');
-  } else if (pageQueryData['PAGE_ACCESS_RELATIVE_URL'] != input["PAGE_ACCESS_RELATIVE_URL"] && input["PAGE_ACCESS_RELATIVE_URL"].length == 0) {
-    await updateIsPureNavigationStep('Yes');
-  }
-
-  let testSetListQuery = `SELECT ts.TEST_SET_UUID,p.PROCESS_NAME, ua.USER_ACTION_NAME FROM TEST_SET ts JOIN PROCESS p ON ts.PROCESS_UUID = p.PROCESS_UUID JOIN VIEW_USER_ACTION ua ON ts.USER_ACTION_UUID = ua.USER_ACTION_UUID WHERE ts.PAGE_UUID=:PAGE_UUID`;
-  let testSetListQueryData = await serviceOrchestrator.selectRecordsUsingQuery("PRIMARYSPRINGFM", testSetListQuery, input);
-  for (let data of testSetListQueryData) {
-    let object = {};
-    object['TEST_SET_UUID'] = data['TEST_SET_UUID'];
-    object['TEST_SET_NAME'] = data['PROCESS_NAME'] + ' - ' + input['PAGE_NAME'] + ' - ' + data['USER_ACTION_NAME'];
-    testSetlist.push(object);
-  }
-
-  input["AppEngChildEntity:TEST_SET_NEW"] = testSetlist;
-  input["AppEngChildEntity:TEST_CASE_STEP_NEW"] = testCaseStepList;
-  input["AppEngChildEntity:TEST_CASE_FUNCTION_STEP"] = testCaseFunctionStepList;
-  input["AppEngChildEntity:FUNCTION_STEP"] = functionStepList;
-} else if (input.compositeEntityAction == 'Delete') {
-  // UI Element
-  let uiElementsQuery = `SELECT UI_ELEMENT_UUID FROM UI_ELEMENT WHERE PAGE_NEW_UUID = :PAGE_UUID;`;
-  let uiElementsQueryData = await serviceOrchestrator.selectRecordsUsingQuery("PRIMARYSPRINGFM", uiElementsQuery, input);
-
-  for (data of uiElementsQueryData) {
-    deleteRecord('UI_ELEMENT_UUID', data['UI_ELEMENT_UUID'], 'UI_ELEMENT', input.APP_LOGGED_IN_FUNTIONAL_AREA_ID);
-  }
-
-  // for View UI Element
-  let viewUiElementsQuery = `SELECT VIEW_UI_ELEMENT_UUID FROM VIEW_UI_ELEMENT WHERE PAGE_UUID = :PAGE_UUID`;
-  let viewUiElementsQueryData = await serviceOrchestrator.selectRecordsUsingQuery("PRIMARYSPRINGFM", viewUiElementsQuery, input);
-
-  for (data of viewUiElementsQueryData) {
-    deleteRecord('VIEW_UI_ELEMENT_UUID', data['VIEW_UI_ELEMENT_UUID'], 'VIEW_UI_ELEMENT', input.APP_LOGGED_IN_FUNTIONAL_AREA_ID);
-  }
-
-
-  // for View Navigation Step
-  let viewNavigationStepQuery = `SELECT VIEW_NAVIGATION_STEP_UUID FROM VIEW_NAVIGATION_STEP WHERE VIEW_UUID IN (SELECT VIEW_UUID FROM PAGE_VIEW WHERE PAGE_UUID = :PAGE_UUID);`;
-  let viewNavigationStepQueryData = await serviceOrchestrator.selectRecordsUsingQuery("PRIMARYSPRINGFM", viewNavigationStepQuery, input);
-
-  for (data of viewNavigationStepQueryData) {
-    deleteRecord('VIEW_NAVIGATION_STEP_UUID', data['VIEW_NAVIGATION_STEP_UUID'], 'VIEW_NAVIGATION_STEP', input.APP_LOGGED_IN_FUNTIONAL_AREA_ID);
-  }
-
-  // for View Navigation Step Attribute Value
-  let viewNavigationStepAttributeValueQuery = `SELECT VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE_UUID FROM VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE WHERE VIEW_UUID IN (SELECT VIEW_UUID FROM PAGE_VIEW WHERE PAGE_UUID = :PAGE_UUID);`;
-  let viewNavigationStepAttributeValueQueryData = await serviceOrchestrator.selectRecordsUsingQuery("PRIMARYSPRINGFM", viewNavigationStepAttributeValueQuery, input);
-
-  for (data of viewNavigationStepAttributeValueQueryData) {
-    deleteRecord('VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE_UUID', data['VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE_UUID'], 'VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE', input.APP_LOGGED_IN_FUNTIONAL_AREA_ID);
-  }
-
-  let pageViewQuery = `SELECT VIEW_UUID FROM PAGE_VIEW WHERE PAGE_UUID=:PAGE_UUID`;
-  let pageViewQueryData = await serviceOrchestrator.selectRecordsUsingQuery("PRIMARYSPRINGFM", pageViewQuery, input);
-
-  for (data of pageViewQueryData) {
-    deleteRecord('VIEW_UUID', data['VIEW_UUID'], 'PAGE_VIEW', input.APP_LOGGED_IN_FUNTIONAL_AREA_ID);
-  }
-
-} else if (input.compositeEntityAction == "Insert" || input.compositeEntityAction == "Save") {
-  let data = {};
-  // let pageID = uuid();
-  // input["PAGE_UUID"] = pageID;
-  // data["PAGE_UUID"] = pageID;
-  data["VIEW_NAME"] = 'Default View';
-  data["IS_DEFAULT_VIEW"] = 'Yes';
-  pageViewList.push(data);
+  input["AppEngChildEntity:VIEW_NAVIGATION_STEP"] = VIEW_NAVIGATION_STEP;
+  input["AppEngChildEntity:VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE"] = VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE;
 }
-
-input["AppEngChildEntity:UI_ELEMENT"] = UI_ELEMENT;
-input["AppEngChildEntity:VIEW_UI_ELEMENT_CHILD_OF_VIEW_UI_ELEMENT"] = VIEW_UI_ELEMENT;
-input["AppEngChildEntity:VIEW_NAVIGATION_STEP"] = VIEW_NAVIGATION_STEP;
-input["AppEngChildEntity:VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE"] = VIEW_NAVIGATION_STEP_ATTRIBUTE_VALUE;
-input["AppEngChildEntity:PAGE_VIEW"] = pageViewList;

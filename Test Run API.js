f7b34d1acd720ab7d584abc1ae5568eb5de11223
Dheeraj-at-381491913668 <dheeraj.manwani@infoorigin.com>
2025-07-01 14:21:07 +0530

    sdvdf

--- Test Run API.js
+++ Test Run API.js
@@ -1,11 +1,5 @@
-debugger;
 try {
-  let input = Object.assign(
-    msg.payload.apiRequestBody?.baseEntity?.records[0]
-      ? msg.payload.apiRequestBody?.baseEntity?.records[0]
-      : msg.payload.apiRequestBody,
-    msg.payload.referenceData
-  );
+  let input = Object.assign(msg.payload.apiRequestBody?.baseEntity?.records[0] ? msg.payload.apiRequestBody?.baseEntity?.records[0] : msg.payload.apiRequestBody, msg.payload.referenceData);
   let sourceType = input.SOURCE_TYPE;
   let testUUUID = '';
   switch (sourceType) {
@@ -27,11 +21,14 @@ try {
     case 'NAVIGATION_STEPS':
       testUUUID = input['VIEW_UUID'];
       break;
+    case 'FEATURE_TEST_SET':
+      testUUUID = input['TEST_SET_UUID'];
+      break;
     default:
       throw new Error(`Unsupported SOURCE_TYPE: ${sourceType}`);
   }
   let data = {
-    Type: input.SOURCE_TYPE,
+    TYPE: input.SOURCE_TYPE,
     APP_LOGGED_IN_FUNTIONAL_AREA_ID: input.APP_LOGGED_IN_FUNTIONAL_AREA_ID,
     APPLICATION_ENVIRONMENT_BASE_URL: input.APPLICATION_ENVIRONMENT_BASE_URL,
     TEST_UUID: testUUUID,
@@ -40,30 +37,16 @@ try {
     MAX_TIMEOUT: input.MAX_TIMEOUT,
     API_POLL_INTERVAL: input.API_POLL_INTERVAL,
     TEST_RUN_UUID: input.TEST_RUN_UUID,
-    APPLICATION_ENVIRONMENT_UUID: input.APPLICATION_ENVIRONMENT_UUID,
+    APPLICATION_ENVIRONMENT_UUID: input.APPLICATION_ENVIRONMENT_UUID
   };
   const url = env.get('AUTOMATION_URL');
+  const final_url = `${url}/execute`;
   msg.payload.connectorType = 'rally';
   msg.payload.methodType = 'update';
-  msg.payload.url = url;
+  msg.payload.url = final_url;
   msg.payload.data = { data: data };
   node.send(msg);
 } catch (t) {
   console.log('Errorr Occured', t.message);
-  return;
-}
-
-// --------------------------
-
-try {
-  let input = Object.assign(msg.payload, msg.payload.referenceData);
-  if (input.responseFromThirdPartyAPI?.response?.message) {
-    msg.payload.result = { mode: 'closeModal', message: input.responseFromThirdPartyAPI.response.message };
-  } else if (input.responseFromThirdPartyAPI?.response?.error) {
-    msg.payload.result = { mode: 'closeModal', message: input.responseFromThirdPartyAPI.response.error };
-  }
-  node.send(msg);
-} catch (error) {
-  console.log('Error Occurred:', error.message);
   return;
 }
